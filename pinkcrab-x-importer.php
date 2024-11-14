935312867847fa20a866ddb9441822579782a00b
Glynn Quelch <glynn.quelch@gmail.com>
2024-11-14 22:43:28 +0000

    Setup the base app setup

--- pinkcrab-x-importer.php
+++ pinkcrab-x-importer.php
@@ -9,3 +9,44 @@
  * Requires at least: 6.1
  * Requires PHP: 7.4
  */
+
+use PinkCrab\BladeOne\BladeOne;
+use PinkCrab\BladeOne\BladeOne_Engine;
+use PinkCrab\BladeOne\PinkCrab_BladeOne;
+use PinkCrab\Perique\Application\App_Factory;
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$app = ( new App_Factory() )
+->module( \PinkCrab\Queue\Module\Perique_Queue::class )
+->module( \PinkCrab\Perique_Admin_Menu\Module\Admin_Menu::class )
+	->module(
+		BladeOne::class,
+		function ( BladeOne $blade ): BladeOne {
+			$blade->config(
+				function ( BladeOne_Engine $engine ) {
+					$engine->directive(
+						'__',
+						function ( $e ) {
+							return "<?php echo __( $e, 'pc-cm' ); ?>";
+						}
+					)
+					->set_comment_mode( PinkCrab_BladeOne::COMMENT_NONE )
+					->set_mode( PinkCrab_BladeOne::MODE_SLOW );
+					return $engine;
+				}
+			);
+
+			return $blade;
+		}
+	)
+	->default_setup()
+	->di_rules( require __DIR__ . '/config/dependencies.php' )
+	->app_config( require __DIR__ . '/config/settings.php' )
+	->registration_classes( require __DIR__ . '/config/registration.php' )
+->boot();
